9692a7ce4d0fbe7a9270417bc099bac2bb9f0629
simplyObare <josephatowiny@gmail.com>
2025-03-23 16:07:58 +0300

    Setting up the React JS Frontend

--- Django React App/backend/crud/settings.py
+++ Django React App/backend/crud/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "api",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -38,6 +40,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173/",
+]
+
 
 ROOT_URLCONF = "crud.urls"
 
---
+++ Django React App/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Our Application</p>
+      </div>
+    </>
+  )
+}
+
+export default App
