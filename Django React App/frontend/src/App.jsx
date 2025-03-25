55892bc1beccd50546db3d78e10539c15ae7f423
simplyObare <josephatowiny@gmail.com>
2025-03-25 11:48:42 +0300

    Creating a Responsive Navigation Menu with Material UI Components

--- Django React App/frontend/src/App.jsx
+++ Django React App/frontend/src/App.jsx
@@ -3,15 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
 import Create from './components/Create.jsx'
+import NavBar from './components/NavBar.jsx'
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <NavBar
+        drawerWidth={240}
+        content={
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        }
+      />
     </div>
   )
 }
